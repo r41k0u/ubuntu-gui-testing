36b40eba1fbb7864e1beb6df732e717ea1362fda
Pragyansh Chaturvedi <pragyansh.chaturvedi@canonical.com>
2025-07-03 14:37:26 +0530

    rpi: add shebang to plucky/hotplug/07-usb/install_img.py, rename usb device and mountpoint

--- tests/raspberry-pi/plucky/hotplug/07-usb/install_img.py
+++ tests/raspberry-pi/plucky/hotplug/07-usb/install_img.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # This is meant to run on the TS to install or remove an image on a USB drive.
 
 import os
@@ -12,20 +13,20 @@ if sys.argv[1] not in ["install", "remove"]:
     print("Invalid argument. Use 'install' or 'remove'.")
     sys.exit(1)
 
-if not os.path.exists("/mnt/usb"):
-    subprocess.run(["sudo", "mkdir", "-p", "/mnt/usb"], check=True)
+if not os.path.exists("usb"):
+    subprocess.run(["mkdir", "-p", "usb"], check=True)
 
-if not os.path.ismount("/mnt/usb"):
+if not os.path.ismount("usb"):
     print("Mounting USB drive...")
-    subprocess.run(["sudo", "mount", "/dev/sda1", "/mnt/usb"], check=True)
+    subprocess.run(["sudo", "mount", "/dev/sdb1", "usb"], check=True)
 
 if sys.argv[1] == "install":
     print("Installing image...")
-    subprocess.run(["sudo", "wget", "https://assets.ubuntu.com/v1/a7e3c509-Canonical%20Ubuntu.svg", "-O", "/mnt/usb/ubuntu.svg"], check=True)
-    subprocess.run(["sudo", "umount", "/mnt/usb"], check=True)
+    subprocess.run(["wget", "https://assets.ubuntu.com/v1/a7e3c509-Canonical%20Ubuntu.svg", "-O", "usb/ubuntu.svg"], check=True)
+    subprocess.run(["sudo", "umount", "usb"], check=True)
     print("Image installed successfully.")
 elif sys.argv[1] == "remove":
     print("Removing image...")
-    subprocess.run(["sudo", "rm", "-f", "/mnt/usb/ubuntu.svg"], check=True)
-    subprocess.run(["sudo", "umount", "/mnt/usb"], check=True)
+    subprocess.run(["sudo", "rm", "-f", "usb/ubuntu.svg"], check=True)
+    subprocess.run(["sudo", "umount", "usb"], check=True)
     print("Image removed successfully.")
